994f5d9a1c841f0b4108db202c3e1a2d319b10e0
Dani <d.tarazoma@geekshubs.com>
2023-06-05 15:31:44 +0200

    feat: register user

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const db = require('./db');
+const { User } = require('./models');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/health', (req, res) => {
     return res.send('healthy');
 });
+
+app.post('/register', async(req, res) => {
+    try {
+        if (req.body.password.length < 4) {
+            return res.send('Password must be longer than 4 characters');
+        }
+
+        const newPassword = bcrypt.hashSync(req.body.password, 8);
+
+        const newUser = await User.create(
+            {
+                name: req.body.name,
+                email: req.body.email,
+                password: newPassword,
+                role_id: 1
+            }
+        );
+
+        return res.send(newUser);
+    } catch (error) {
+        return res.send('Something went wrong creating users ' + error.message)
+    }
+})
 
 db.then(() =>
     {
@@ -18,5 +45,4 @@ db.then(() =>
 ).catch((error) => {
     console.error('Error starting server', error.message);
 })
-
 
