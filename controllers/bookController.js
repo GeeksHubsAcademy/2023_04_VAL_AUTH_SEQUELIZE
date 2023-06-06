c8864a610ba609c5da2034b72aa2080035454058
Dani <d.tarazoma@geekshubs.com>
2023-06-06 17:29:17 +0200

    feat: destroy book by id

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const { Book } = require("../models");
+const authController = require("./authController");
 
 const bookController = {};
 
@@ -25,6 +26,81 @@ bookController.createBook = async(req, res) => {
             {
                 success: false,
                 message: "Book cant be created",
+                error: error
+            }
+        )
+    }
+}
+
+bookController.updateBook = async(req, res) => {
+    try {
+        const bookId = req.params.id;
+
+        const book = await Book.findByPk(bookId);
+
+        if (!book) {
+            return res.json(
+                {
+                    success: true,
+                    message: "Book doesnt exists"
+                }
+            );
+        };
+
+        const { title, description } = req.body;
+
+        const bookUpdated = await Book.update(
+            {
+                title: title,
+                description: description
+            },
+            {
+                where: {
+                    id: bookId
+                }
+            }
+        )
+
+        return res.json(
+            {
+                success: true,
+                message: "Book updated",
+                data: bookUpdated
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Book cant be updated",
+                error: error
+            }
+        )
+    }
+}
+
+bookController.deleteBook = async(req, res) => {
+    try {
+        const bookId = req.params.id;
+
+        const deleteBook = await Book.destroy({
+            where: {
+                id: bookId
+            }
+        })
+
+        return res.json(
+            {
+                success: true,
+                message: "Book deleted successfully",
+                data: deleteBook
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "Book cant be deleted",
                 error: error
             }
         )
--- views/bookRoutes.js
+++ views/bookRoutes.js
@@ -1,3 +1,4 @@
+const authController = require('../controllers/authController');
 const bookController = require('../controllers/bookController');
 const isAdmin = require('../middlewares/isAdmin');
 const auth = require('../middlewares/verifyToken');
@@ -5,5 +6,7 @@ const auth = require('../middlewares/verifyToken');
 const router = require('express').Router();
 
 router.post('/', auth, isAdmin, bookController.createBook)
+router.put('/:id', auth, isAdmin, bookController.updateBook)
+router.delete('/:id', auth, isAdmin, bookController.deleteBook)
 
 module.exports = router;
