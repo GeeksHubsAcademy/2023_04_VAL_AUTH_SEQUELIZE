980fc9c0de363ce069c0d2a20c6fdc894f3c6831
Dani <d.tarazoma@geekshubs.com>
2023-06-06 10:02:10 +0200

    chore: import only properties into authRoutes

--- views/authRoutes.js
+++ views/authRoutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController')
+// importamos solo register
+// const { register} = require('../controllers/authController')
 
 router.post('/register', authController.register);
+
+// importamos solo register
+// router.post('/register', register);
 
 module.exports = router;
