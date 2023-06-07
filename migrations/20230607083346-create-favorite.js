abef41496a325d3e992e235b735b97a53bf98650
Dani <d.tarazoma@geekshubs.com>
2023-06-07 10:48:16 +0200

    feat: favorite model created and migration

---
+++ migrations/20230607083346-create-favorite.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      book_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Books",
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Favorites');
+  }
+};
